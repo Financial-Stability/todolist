36401e962b80a741456d6c229651cb56ed188f96
Avery2 <JustinAveryChan@gmail.com>
2020-04-22 22:33:36 -0400

    refactoring

--- todolist/application/Main.java
+++ todolist/application/Main.java
@@ -8,19 +8,27 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class GUIManager extends Application {
+public class Main extends Application {
 
-  private List<String> args;
   private static final int WINDOW_WIDTH = 500;
   private static final int WINDOW_HEIGHT = 500;
   private static final String APP_TITLE = "Todo App";
 
   @Override
   public void start(Stage stage) throws Exception {
-    args = this.getParameters().getRaw();
 
+    // scene
+    Scene mainScene = new Scene(createPane(), WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    stage.setTitle("JavaFX Application");
+    stage.setScene(mainScene);
+    stage.show();
+  }
+
+  private Pane createPane() {
     // components
     Label topLabel = new Label(APP_TITLE);
     String letters[] = {"A", "B", "C", "D", "E"};
@@ -34,12 +42,7 @@ public class GUIManager extends Application {
     root.setLeft(combo_box);
     root.setBottom(button);
 
-    // scene
-    Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-    
-    stage.setTitle("JavaFX Application");
-    stage.setScene(mainScene);
-    stage.show();
+    return root;
 
   }
 
