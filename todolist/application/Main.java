55a5c494c6386149269d7f1f625f31a833b5855b
Avery2 <JustinAveryChan@gmail.com>
2020-04-22 22:56:50 -0400

    start putting things in

--- todolist/application/Main.java
+++ todolist/application/Main.java
@@ -1,14 +1,17 @@
 package application;
 
-import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -22,28 +25,47 @@ public class Main extends Application {
 
     // scene
     Scene mainScene = new Scene(createPane(), WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+    mainScene.getStylesheets().add(getClass().getResource("stylesheet.css").toString());
 
-    stage.setTitle("JavaFX Application");
+    stage.setTitle(APP_TITLE);
     stage.setScene(mainScene);
     stage.show();
   }
 
   private Pane createPane() {
     // components
-    Label topLabel = new Label(APP_TITLE);
     String letters[] = {"A", "B", "C", "D", "E"};
     ComboBox<String> combo_box = new ComboBox<String>(FXCollections.observableArrayList(letters));
     Button button = new Button("Done");
+    MenuBar menuBar = new MenuBar();
+    VBox settings = new VBox();
+    
+    // apply ids for styling
+    settings.setId("settings_panel");
+
+    // do menu stuff
+
+    final Menu menu = new Menu("Menu");
+    final MenuItem toggle = new MenuItem("Toggle Settings");
+    final MenuItem view1 = new MenuItem("view1");
+    final MenuItem view2 = new MenuItem("view2");
+    final MenuItem view3 = new MenuItem("view3");
+    final Menu help = new Menu("Help");
+    
+    menu.getItems().addAll(toggle, view1, view2, view3);
+    menuBar.getMenus().addAll(menu, help);
+    
+    settings.getChildren().addAll(combo_box);
 
     // pane
     BorderPane root = new BorderPane();
 
-    root.setTop(topLabel);
-    root.setLeft(combo_box);
-    root.setBottom(button);
+    root.setTop(menuBar);
+    root.setLeft(settings);
+    root.setCenter(button);
 
     return root;
-
   }
 
   public static void main(String[] args) {
